0f214672315781559a34c34b4277c0a13066c1e6
Amir Azhar <amir97azhar@gmail.com>
2021-07-15 10:54:16 +0800

    Import lax.js

--- src/screens/Home.js
+++ src/screens/Home.js
@@ -1,7 +1,41 @@
 import "./home.css";
 
+import lax from "lax.js";
+import { useEffect } from "react";
+
 const Home = () => {
-  return <div>Test</div>;
+  useEffect(() => {
+    // Setup lax
+    lax.init();
+
+    lax.addDriver("scrollY", function () {
+      return window.scrollY;
+    });
+
+    // Add your elements
+    lax.addElements(".fadeOut", {
+      scrollY: {
+        opacity: [
+          [0, 300],
+          [1, 0],
+        ],
+      },
+    });
+    lax.addElements(".fadeIn", {
+      scrollY: {
+        opacity: [
+          [300, 600],
+          [0, 1],
+        ],
+      },
+    });
+  }, []);
+  return (
+    <div>
+      <h1 className="fadeOut">Hello</h1>
+      <h1 className="fadeIn">I'm Amir</h1>
+    </div>
+  );
 };
 
 export default Home;
